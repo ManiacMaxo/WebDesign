34e002e4643fc0cd5c837afa69cdac3243ba305f
Victor Gorchilov <victor@gorchilov.com>
2020-06-04 14:49:56 +0300

    [up] initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp")
+const sass = require("gulp-sass")
+const browserSync = require("browser-sync").create()
+
+const style = () => {
+    return gulp
+        .src("./src/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./src/css"))
+
+        .pipe(browserSync.stream())
+}
+
+exports.watch = () => {
+    browserSync.init({
+        server: { baseDir: "./src" },
+    })
+    gulp.watch("./src/scss/**/*.scss", style)
+    gulp.watch("./src/*.html").on("change", browserSync.reload)
+    gulp.watch("./src/js/**/*.js").on("change", browserSync.reload)
+}
