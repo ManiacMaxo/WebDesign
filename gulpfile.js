962d96ccb2e34456cc173068a34227bf1d298708
Victor Gorchilov <victor@gorchilov.com>
2020-06-04 17:02:00 +0300

    [up] add styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@ const gulp = require("gulp")
 const sass = require("gulp-sass")
 const browserSync = require("browser-sync").create()
 
+// sass to css
 const style = () => {
     return gulp
-        .src("./src/scss/**/*.scss")
+        .src("./src/sass/**/*.sass")
         .pipe(sass())
         .pipe(gulp.dest("./src/css"))
-
         .pipe(browserSync.stream())
 }
 
+// start server, watch files for changes
 exports.watch = () => {
     browserSync.init({
         server: { baseDir: "./src" },
     })
-    gulp.watch("./src/scss/**/*.scss", style)
+    gulp.watch("./src/sass/**/*.sass", style) // on change update css
     gulp.watch("./src/*.html").on("change", browserSync.reload)
     gulp.watch("./src/js/**/*.js").on("change", browserSync.reload)
 }
