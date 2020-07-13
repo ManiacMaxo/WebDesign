ed92713948bac404fcf49353f80ec36ee9b4d853
Victor Gorchilov <victor@gorchilov.com>
2020-07-13 12:08:28 +0300

    [up] add good pictures

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,7 @@ burger.addEventListener('click', () => {
     menu.classList.toggle('hidden')
     burger.classList.toggle('active')
 })
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
--- vhosts.js
+++ vhosts.js
@@ -5,6 +5,8 @@ const db = require('./util/db')
 const server = express()
 
 server.use(vhost('bagun.local', require('./app')))
+server.use(vhost('bagun.gorchilov.net', require('./app')))
+server.use(vhost('gorchilov.net', require('./app')))
 
 db.connect().then(() => {
     console.log('connected')
