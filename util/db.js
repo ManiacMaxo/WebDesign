f84334b30c5a1738c82682a062ea3f5b1a2cbdcf
Victor <victor@gorchilov.com>
2020-10-13 13:54:38 +0000

    [up] change to postgresql

--- util/db.js
+++ util/db.js
@@ -1,9 +1,6 @@
-const mysql = require('mysql')
-const util = require('util')
+const { Client } = require('pg')
 const { config } = require('../secret')
 
-const db = mysql.createConnection(config)
-db.query = util.promisify(db.query).bind(db)
-db.connect = util.promisify(db.connect).bind(db)
+client = new Client(config)
 
-module.exports = db
+module.exports = client
